8cdb958a976d067751954d463585915aaa882be0
Marouan <marouan_k@live.be>
2019-12-24 21:49:01 +0100

    ajout du state, modification contact et contactList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Contact from './components/contact'
 import ContactList from './components/contactList'
 
 
--- src/components/contact.js
+++ src/components/contact.js
@@ -2,24 +2,55 @@ import React from 'react';
 import './contact.css';
 //import { checkPropTypes } from 'prop-types';
 
-const Contact = (props) => {
-    return(
-        <article className="Contact">
+
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            online : props.status
+         }
+    }
+    render() { 
+        return (
+            <article className="Contact">
             <aside>
-                 <img className="avatar" src={props.avatar}></img>  
+                 <img className="avatar" src={this.props.avatar}></img>  
             </aside>
             <main>
                 <div className="name">
-                    {props.name}
+                    {this.props.name}
                 </div>
-                <div className="status">
-                    <span className={props.status ? 'status-online' : 'status-offline'}></span>
-                    <span className="status-text">{props.status ? 'Online' : 'Offline' }</span>
+                <div  onClick={event => {const statusOnOff = !this.state.online
+                this.setState({online : statusOnOff})}} className="status">
+                    <span className={this.state.online ? 'status-online' : 'status-offline'}></span>
+                    <span className="status-text">{this.state.online ? 'Online' : 'Offline' }</span>
                 </div>
             </main>
         </article>
-        
-    )
+          );
+    }
 }
-
+ 
 export default Contact;
+
+// const Contact = (props) => {
+//     return(
+//         <article className="Contact">
+//             <aside>
+//                  <img className="avatar" src={props.avatar}></img>  
+//             </aside>
+//             <main>
+//                 <div className="name">
+//                     {props.name}
+//                 </div>
+//                 <div className="status">
+//                     <span className={props.status ? 'status-online' : 'status-offline'}></span>
+//                     <span className="status-text">{props.status ? 'Online' : 'Offline' }</span>
+//                 </div>
+//             </main>
+//         </article>
+        
+//     )
+// }
+
+// export default Contact;
